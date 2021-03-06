ba9578f0f23de5d2e22f0ebac5907964abfc6095
tr-41 <tr18300926484@163.com>
2021-03-06 11:02:01 +0800

    3-6-1 ..

--- src/main/java/com/zhuanye/wiki/controller/TestController.java
+++ src/main/java/com/zhuanye/wiki/controller/TestController.java
@@ -1,14 +1,27 @@
 package com.zhuanye.wiki.controller;
 
+import com.zhuanye.wiki.domain.Test;
+import com.zhuanye.wiki.service.TestService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 
 public class TestController {
 
+    @Resource
+    private TestService testService;
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World!Alice";
+    }
+
+    @GetMapping("/list")
+    public List<Test> list(){
+        return testService.list();
     }
 }
---
+++ src/main/java/com/zhuanye/wiki/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.zhuanye.wiki.mapper;
+
+import com.zhuanye.wiki.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+}

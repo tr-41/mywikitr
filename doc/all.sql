032aa21508ed30cee83d7e00fa3e33d46735374f
tr-41 <tr18300926484@163.com>
2021-03-16 08:59:20 +0800

    3-16-1 集成mybatis generator代码，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -1,8 +1,8 @@
-drop table if exists `test`;
-create table `test`(
+drop table if exists `demo`;
+create table `demo`(
     `id` bigint not null comment 'id',
     `name` varchar(50) comment '名称',
-    `password` varchar(50) comment '名称',
     primary key(`id`)
 )engine=innodb default charset=utf8mb4
  comment ='测试';
+insert into demo values (1,'张三');

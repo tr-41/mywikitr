d6215d0986f6ea4394366d5b2fea4d2cc47bb4b4
tr-41 <tr18300926484@163.com>
2021-04-04 15:24:03 +0800

    4-4-1 使用ref和reactive实现数据绑定

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -63,13 +63,16 @@
     <a-layout-content
       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
   >
-    Content
+      <pre>
+      {{ ebooks }}
+      {{ ebooks2 }}
+      </pre>
   </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from 'axios';
 
@@ -77,12 +80,24 @@ export default defineComponent({
   name: 'Home',
   setup(){
     console.log("setup");
-    axios.get("http://localhost:8081/ebook/list?name=Spring").then((response)=>{
-      console.log(response);
-    })
+    const ebooks=ref();
+    const ebooks1=reactive({books:[]});
+    onMounted(()=>{
+      console.log("onMounted");
+      axios.get("http://localhost:8081/ebook/list?name=Spring").then((response)=>{
+        const data=response.data;
+        ebooks.value=data.content;
+        ebooks1.books=data.content;
+        console.log(response);
+      });
+    });
+    return{
+      ebooks,
+      ebooks2: toRef(ebooks1,"books")
+    }
   },
   components: {
     HelloWorld,
-  },
+  }
 });
 </script>

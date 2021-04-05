c999caded730619e2ed163d37cceb0a48f631ecc
tr-41 <tr18300926484@163.com>
2021-04-05 19:21:39 +0800

    4-5-4 axios请求域改为读配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -111,8 +111,8 @@ export default defineComponent({
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
     onMounted(() => {
-      console.log("onMounted");
-      axios.get("http://localhost:8081/ebook/list").then((response) => {
+      console.log("onMounted111");
+      axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;

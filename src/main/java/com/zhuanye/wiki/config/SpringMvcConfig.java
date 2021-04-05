71bd307c3530225be3389867cc3e8035f61e5bdd
tr-41 <tr18300926484@163.com>
2021-04-05 20:00:28 +0800

    4-5-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/zhuanye/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package com.zhuanye.wiki.config;
+
+import com.zhuanye.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}

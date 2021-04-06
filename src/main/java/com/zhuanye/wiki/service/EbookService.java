0da54df6b91f1689677942f9c9d98ac51ba478a2
tr-41 <tr18300926484@163.com>
2021-04-06 09:34:33 +0800

    4-6-5 集成PageHelper插件

--- src/main/java/com/zhuanye/wiki/service/EbookService.java
+++ src/main/java/com/zhuanye/wiki/service/EbookService.java
@@ -1,5 +1,7 @@
 package com.zhuanye.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zhuanye.wiki.domain.Demo;
 import com.zhuanye.wiki.domain.Ebook;
 import com.zhuanye.wiki.domain.EbookExample;
@@ -8,6 +10,8 @@ import com.zhuanye.wiki.mapper.EbookMapper;
 import com.zhuanye.wiki.req.EbookReq;
 import com.zhuanye.wiki.resp.EbookResp;
 import com.zhuanye.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -19,6 +23,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG= LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
@@ -28,7 +34,13 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> eBookList=ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo=new PageInfo<>(eBookList);
+        LOG.info("总行数:{}",pageInfo.getTotal());
+        LOG.info("总页数:{}",pageInfo.getPages());
+
 //        List<EbookResp> respList=new ArrayList<>();
 //        for (Ebook ebook : eBookList) {
 ////            EbookResp ebookResp = new EbookResp();

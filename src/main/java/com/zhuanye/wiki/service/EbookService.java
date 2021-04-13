c0ee38b1041c4975d681c4f3ff1d258fdf181010
tr-41 <tr18300926484@163.com>
2021-04-13 14:30:54 +0800

    4-13-7 完成分类管理

--- src/main/java/com/zhuanye/wiki/service/EbookService.java
+++ src/main/java/com/zhuanye/wiki/service/EbookService.java
@@ -21,7 +21,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
-    private static final Logger LOG= LoggerFactory.getLogger(EbookService.class);
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -29,45 +30,58 @@ public class EbookService {
     @Resource
     private SnowFlake snowFlake;
 
-    public PageResp<EbookQueryResp> list(EbookQueryReq req){
+    public PageResp<EbookQueryResp> list(EbookQueryReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        if(!ObjectUtils.isEmpty(req.getName())) {
+        if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
-        PageHelper.startPage(req.getPage(),req.getSize());
-        List<Ebook> eBookList=ebookMapper.selectByExample(ebookExample);
+        if (!ObjectUtils.isEmpty(req.getCategoryId2())) {
+            criteria.andCategory2IdEqualTo(req.getCategoryId2());
+        }
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        PageInfo<Ebook> pageInfo=new PageInfo<>(eBookList);
-        LOG.info("总行数:{}",pageInfo.getTotal());
-        LOG.info("总页数:{}",pageInfo.getPages());
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
-//        List<EbookResp> respList=new ArrayList<>();
-//        for (Ebook ebook : eBookList) {
-////            EbookResp ebookResp = new EbookResp();
-////            BeanUtils.copyProperties(ebook,ebookResp);
-//            EbookResp ebookResp=CopyUtil.copy(ebook,EbookResp.class);
-//        }
+        // List<EbookResp> respList = new ArrayList<>();
+        // for (Ebook ebook : ebookList) {
+        //     // EbookResp ebookResp = new EbookResp();
+        //     // BeanUtils.copyProperties(ebook, ebookResp);
+        //     // 对象复制
+        //     EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //
+        //     respList.add(ebookResp);
+        // }
 
-        List<EbookQueryResp> list = CopyUtil.copyList(eBookList, EbookQueryResp.class);
-        PageResp<EbookQueryResp> pageResp=new PageResp();
+        // 列表复制
+        List<EbookQueryResp> list = CopyUtil.copyList(ebookList, EbookQueryResp.class);
+
+        PageResp<EbookQueryResp> pageResp = new PageResp();
         pageResp.setTotal(pageInfo.getTotal());
         pageResp.setList(list);
+
         return pageResp;
     }
 
-    public void save(EbookSaveReq req){
-        Ebook ebook=CopyUtil.copy(req,Ebook.class);
-        if(ObjectUtils.isEmpty(req.getId())){
+    /**
+     * 保存
+     */
+    public void save(EbookSaveReq req) {
+        Ebook ebook = CopyUtil.copy(req, Ebook.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
             ebook.setId(snowFlake.nextId());
-            //新增
             ebookMapper.insert(ebook);
-        }else {
+        } else {
+            // 更新
             ebookMapper.updateByPrimaryKey(ebook);
         }
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         ebookMapper.deleteByPrimaryKey(id);
     }
 }
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -32,9 +32,9 @@
         <template #cover="{ text: cover }">
           <img v-if="cover" :src="cover" alt="avatar" />
         </template>
-<!--        <template v-slot:category="{ text, record }">-->
-<!--          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>-->
-<!--        </template>-->
+        <template v-slot:category="{ text, record }">
+          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+        </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
 <!--            <router-link :to="'/admin/doc?ebookId=' + record.id">-->
@@ -241,54 +241,48 @@ export default defineComponent({
     };
 
     const level1 =  ref();
-    //let categorys: any;
+    let categorys: any;
     /**
      * 查询所有分类
      **/
-    // const handleQueryCategory = () => {
-    //   loading.value = true;
-    //   axios.get("/category/all").then((response) => {
-    //     loading.value = false;
-    //     const data = response.data;
-    //     if (data.success) {
-    //       categorys = data.content;
-    //       console.log("原始数组：", categorys);
-    //
-    //       level1.value = [];
-    //       level1.value = Tool.array2Tree(categorys, 0);
-    //       console.log("树形结构：", level1.value);
-    //
-    //       // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
-    //       handleQuery({
-    //         page: 1,
-    //         size: pagination.value.pageSize,
-    //       });
-    //     } else {
-    //       message.error(data.message);
-    //     }
-    //   });
-    // };
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
 
-    // const getCategoryName = (cid: number) => {
-    //   // console.log(cid)
-    //   let result = "";
-    //   categorys.forEach((item: any) => {
-    //     if (item.id === cid) {
-    //       // return item.name; // 注意，这里直接return不起作用
-    //       result = item.name;
-    //     }
-    //   });
-    //   return result;
-    // };
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
 
-    // onMounted(() => {
-    //   handleQueryCategory();
-    // });
-    onMounted(() => {
-      handleQuery({
-        page:1,
-        size:pagination.value.pageSize
+          // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
+          handleQuery({
+            page: 1,
+            size: pagination.value.pageSize,
+          });
+        } else {
+          message.error(data.message);
+        }
       });
+    };
+
+    const getCategoryName = (cid: number) => {
+      // console.log(cid)
+      let result = "";
+      categorys.forEach((item: any) => {
+        if (item.id === cid) {
+          // return item.name; // 注意，这里直接return不起作用
+          result = item.name;
+        }
+      });
+      return result;
+    };
+
+    onMounted(() => {
+      handleQueryCategory();
     });
 
     return {
@@ -299,6 +293,7 @@ export default defineComponent({
       loading,
       handleTableChange,
       handleQuery,
+      getCategoryName,
 
       edit,
       add,

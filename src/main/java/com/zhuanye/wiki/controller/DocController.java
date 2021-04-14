ac3de3ad27daf0605e953ca21d3b54de5e9d433d
tr-41 <tr18300926484@163.com>
2021-04-14 21:48:55 +0800

    4-14-1 完后文档管理增删改查的前后端代码

---
+++ src/main/java/com/zhuanye/wiki/controller/DocController.java
@@ -0,0 +1,51 @@
+package com.zhuanye.wiki.controller;
+
+import com.zhuanye.wiki.req.DocQueryReq;
+import com.zhuanye.wiki.req.DocSaveReq;
+import com.zhuanye.wiki.resp.DocQueryResp;
+import com.zhuanye.wiki.resp.CommonResp;
+import com.zhuanye.wiki.resp.PageResp;
+import com.zhuanye.wiki.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq req) {
+        CommonResp<PageResp<DocQueryResp>> resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        docService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+}
---
+++ src/main/java/com/zhuanye/wiki/req/DocQueryReq.java
@@ -0,0 +1,8 @@
+package com.zhuanye.wiki.req;
+
+public class DocQueryReq extends PageReq{
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
